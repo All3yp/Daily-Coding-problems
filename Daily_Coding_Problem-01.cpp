8464f9b9ae782a6d593f87404c42a7adbc8b6076
All3yp <noyallepereira@gmail.com>
2022-09-16 14:48:44 -0300

    chore: changing style, removing comments

--- Daily_Coding_Problem-01.cpp
+++ Daily_Coding_Problem-01.cpp
@@ -1,15 +1,5 @@
-/*
-    🌽  Daily Coding Problem: Problem #1 [Easy] 🌽
+/* MARK: - SOLUTION ✨ */
 
-This problem was recently asked by Google.
-Given a list of numbers and a number k, return whether any two numbers 
-rom the list add up to k.
-For example, given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17.
-
-Bonus: Can you do this in one pass?
-*/
-
-// SOLUTION ✨
 #include <iostream>
 using namespace std;
 
@@ -26,6 +16,7 @@ bool listAdd(const int list[], int k, int size)
     return false;
 }
 
+/* MARK: - Main func */
 int main()
 {
     int list[4] = {10, 15, 3, 7};
